0cdbd84c0e776baf5b0320c25eb1829215d26942
EC2 Default User <ec2-user@ip-172-31-90-146.ec2.internal>
2022-03-10 09:34:36 +0000

    [Finish]12章

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :post_images, only: [:new, :index, :show, :create]
+  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:show, :edit]
 
   get 'homes/about' => 'homes#about', as:'about'
   #このサイトのtestアカウント pass: test12345
